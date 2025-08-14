8fefe8156c39141a9f453ce3d9ac38715ef81913
Gobinda2026 <gobindakumr123@gmail.com>
2025-08-14 22:30:59 +0530

    Add files via upload

---
+++ firebaseaut.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDuDb7YbuhftmvYJgJ7i3qDLvPXIMjwROY",
+    authDomain: "repidoc-3b4ce.firebaseapp.com",
+    projectId: "repidoc-3b4ce",
+    storageBucket: "repidoc-3b4ce.firebasestorage.app",
+    messagingSenderId: "433248893926",
+    appId: "1:433248893926:web:d2adb296bd64e78b127427"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
